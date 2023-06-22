0b3ae17b0f860d551ce9faa86e2e4e31cb5b4831
Momchil Enchev <m.s.enchev@gmail.com>
2023-06-23 00:29:56 +0300

    finalize forms

---
+++ My_Music_App/user_profile/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from My_Music_App.user_profile.models import Profile
+
+
+class ProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'age': forms.NumberInput(attrs={'placeholder': 'Age'})
+        }
