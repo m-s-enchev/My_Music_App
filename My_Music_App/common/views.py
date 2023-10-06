471f481b333730e84e0595da445523ba4b8398f9
Momchil Enchev <m.s.enchev@gmail.com>
2023-10-06 20:36:52 +0300

    added search results page

--- My_Music_App/common/views.py
+++ My_Music_App/common/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from My_Music_App.album.models import Album
+from My_Music_App.common.forms import SearchForm
 from My_Music_App.user_profile.models import Profile
 from My_Music_App.user_profile.forms import ProfileModelForm
 
@@ -11,17 +12,26 @@ def homepage(request):
     profile = Profile.objects.first()
     albums = Album.objects.all()
     form = ProfileModelForm()
+    search_form = SearchForm()
 
     if request.method == 'POST':
-        form = ProfileModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(request.path_info)
+        print(request.POST)
+        if 'profile_form' in request.POST:
+            form = ProfileModelForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(request.path_info)
+        elif 'search_form' in request.POST:
+            search_form = SearchForm(request.POST)
+            if search_form.is_valid():
+                print("Success")
+                return redirect('search_results')
 
     context = {
         'profile': profile,
         'albums': albums,
-        'form': form
+        'form': form,
+        'search_form': search_form,
     }
 
     if profile:
@@ -32,5 +42,9 @@ def homepage(request):
     return render(request, template_name=template, context=context)
 
 
-
-
+def search_results(request):
+    profile = Profile.objects.first()
+    context = {
+        "profile":profile
+    }
+    return render(request, template_name="common/search-results.html", context=context)
