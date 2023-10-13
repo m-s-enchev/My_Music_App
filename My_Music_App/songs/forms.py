fa925adc24599b9f86b87a70d19578c1f9456d8f
Momchil Enchev <m.s.enchev@gmail.com>
2023-10-13 14:56:47 +0300

    make album a hidden field in SongForm

--- My_Music_App/songs/forms.py
+++ My_Music_App/songs/forms.py
@@ -6,4 +6,7 @@ from My_Music_App.songs.models import Songs
 class SongsForm(forms.ModelForm):
     class Meta:
         model = Songs()
-        fields = ['name', 'length']
+        fields = '__all__'
+        widgets = {
+            'album': forms.HiddenInput(),
+        }
